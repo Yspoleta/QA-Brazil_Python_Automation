0616d425af3a46260fa43edd9e57123f5ccd4b2c
Paulo <paulomsnb@gmail.com>
2025-11-20 15:49:35 -0300

    Commit

---
+++ data.py
@@ -0,0 +1,9 @@
+URBAN_ROUTES_URL = 'https://cnt-7d35d4d4-0451-4a5a-98c6-8543e002c6cf.containerhub.tripleten-services.com?lng=pt'
+
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
